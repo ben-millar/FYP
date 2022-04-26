78c77784a94073d9c2912dea3eab93ae6ae95927
ben-millar <c00236772@itcarlow.ie>
2022-04-26 14:49:04 +0100

    Finished final powerpoint presentation

--- Reinforcement Bin Packing/VM Allocation/VMAllocationEnvironment.py
+++ Reinforcement Bin Packing/VM Allocation/VMAllocationEnvironment.py
@@ -1,4 +1,5 @@
 import csv
+from tokenize import Double
 
 from gym.spaces import Discrete, Box
 import numpy as np
@@ -12,28 +13,28 @@ class VMAllocationEnvironment(object):
         self.capacity   = capacity
 
         self.available_vms = []
+        self.read_data()
 
         self.state = self.reset()
 
-        low = np.hstack((
-            np.full(self.num_bins + 1, 0),
-            np.full(self.num_bins + 1, 0),
-            np.full(self.num_bins + 1, 0),
-            np.full(self.num_bins + 1, 0)
-            )).reshape(self.num_bins, 4)
+        low = np.column_stack((
+            np.full(self.num_bins + 1, np.float32(0)),
+            np.full(self.num_bins + 1, np.float32(0)),
+            np.full(self.num_bins + 1, np.float32(0)),
+            np.full(self.num_bins + 1, np.float32(0))
+            ))
 
-        high = np.hstack((
-            np.full((self.num_bins + 1), self.capacity),
-            np.full((self.num_bins + 1), self.capacity),
-            np.full((self.num_bins + 1), self.capacity),
-            np.full((self.num_bins + 1), self.capacity)
-            )).reshape(self.num_bins + 1, 4)
+        high = np.column_stack((
+            np.full((self.num_bins + 1), np.float32(100)),
+            np.full((self.num_bins + 1), np.float32(self.capacity)),
+            np.full((self.num_bins + 1), np.float32(self.capacity)),
+            np.full((self.num_bins + 1), np.float32(self.capacity))
+            ))
 
         self.observation_space = Box(
             low=low,
             high=high,
-            shape=(self.num_bins+1, 4),
-            dtype=np.int
+            shape=(self.num_bins+1, 4)
         )
 
         # Actions 0-n select a bin, n+1 discards item
@@ -48,40 +49,45 @@ class VMAllocationEnvironment(object):
 
             reader = csv.reader(csv_file, delimiter=',')
 
-            skip_headers = True
-    
+            skip_header = True
+
             for row in reader:
-                if skip_headers:
-                    skip_headers = False
+                if skip_header:
+                    skip_header = False
                     continue
 
-                self.available_vms = row
+                self.available_vms.append([float(i) for i in row])
 
 
     def step(self, action):
         # Discard item
         if (action == self.num_bins):
             info = { 'placed':0, 'misplaced':0, 'discarded':1 }
-            reward = -1
+            reward = -new_item[0]
             self.getNewItem()
-        else:
+        else: # Place item
             new_item    = self.state[self.num_bins]
             chosen_bin  = self.state[action]
 
-            remaining_capacity = chosen_bin - new_item
+            # Value is +ve, sizes are -ve
+            remaining_capacity = chosen_bin + new_item
 
             # If this placement has caused any of our dimensions to go below 0
             if np.any((0 > remaining_capacity)):
                 info = { 'placed':0, 'misplaced':1, 'discarded':0 }
-                reward = -new_item.prod() # Product of x*y
+                reward = -1
             else:
                 info = { 'placed':1, 'misplaced':0, 'discarded':0 }
-                reward = new_item.prod() # Product of x*y
+                reward = new_item[0] # Value
                 self.state[action] = remaining_capacity
                 self.getNewItem()
 
-        # Returns true if the x OR y capacities of all bins are below the minimum item sizes
-        isTerminalState = np.all(self.state[:,0] <= self.min_x) or np.all(self.state[:,1] <= self.min_y)
+        # Returns true if none of our bins have any capacity for any one resource
+        isTerminalState = \
+            np.all(np.take(self.state, 1, axis=1) < 0.01) or \
+            np.all(np.take(self.state, 2, axis=1) < 0.01) or \
+            np.all(np.take(self.state, 3, axis=1) < 0.01)
+
 
         return self.state, reward, isTerminalState, info
 
@@ -93,12 +99,12 @@ class VMAllocationEnvironment(object):
     def reset(self):
         # An [n, 4] array representing the value, CPU, Memory and Storage capacities
         # of each of our bins, with an extra bin at the end representing the next VM.
-        self.state = np.hstack((
-            np.full(self.num_bins + 1, 0), # Value
-            np.full(self.num_bins + 1, self.capacity), # CPU
-            np.full(self.num_bins + 1, self.capacity), # Memory
-            np.full(self.num_bins + 1, self.capacity) # Storage
-            )).reshape(self.num_bins + 1, 4)
+        self.state = np.column_stack((
+            np.full(self.num_bins + 1, np.float32(0)), # Value
+            np.full(self.num_bins + 1, np.float32(self.capacity)), # CPU
+            np.full(self.num_bins + 1, np.float32(self.capacity)), # Memory
+            np.full(self.num_bins + 1, np.float32(self.capacity)) # Storage
+            ))
 
         self.getNewItem()
 
@@ -106,6 +112,6 @@ class VMAllocationEnvironment(object):
 
     
     def getNewItem(self):
-        index = np.random.randint(low=0, high=len(self.available_vms)-1)
+        index = np.random.randint(low=0, high=len(self.available_vms) - 1)
         next_item = self.available_vms[index][:-1] # We ignore the final, popularity, value for now
         self.state[self.num_bins] = next_item
